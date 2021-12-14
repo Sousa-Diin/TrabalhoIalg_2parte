5dece47a6b1ba08b51fbc4c2c07011367503c6d5
Wildes Sousa <74382948+Sousa-Diin@users.noreply.github.com>
2021-12-14 02:03:37 -0300

    work-app
    entrega do protótipo do algoritmo

--- main.cpp
+++ main.cpp
@@ -1,21 +1,14 @@
 /*Segunda parte do trabalho de IALG
- * Cadastro de Veiculo
+ * Tema do trabalho: " Carro "
+ * Identificador : placa
  * 
- * NOME: Wildes Augusto de Sousa - 
- * Matricula: 202020602
- * NOME: Jonatha Levi dos Santos Lustosa 
- * Matricula: 202020161
+ * NOME: Wildes Augusto de Sousa -                       Matricula: 202020602    Turma:  22A
+ * NOME: Jonatha Levi dos Santos Lustosa -           Matricula: 202020161    Turma:  22A
  * 
- //OBSERVACOES SOBRE O PROGRAMA\\
-
- * ATE O PRESENTE MOMENTO DIA 21/10/2021 AS 18:30hr 
-                              26/10/2021 AS  2:18hr
-                              26/10/2021 AS 13:33hr
-                              27/10/2021 AS  -----
-                              28/10/2021 AS ------
- * O PROGRAMA ESTA FUNCIONANDO PEFEITAMENTO SALVO
- * ALGUMAS FUNCOES QUE AINDA NAO ESTA ESTABELECIDAS 
- * ARQUIVO PRINCIPAL NO GEANY E VERSOES ANTERIORES EM .TXT E  * DOCUMENTO GOOGLE
+ // OBSERVACOES SOBRE O PROGRAMA \\
+ * 
+ * No presente algorito não foi possivel desenvolver algumas funcoes, devido a falta de gestao de tempo
+ * motivos esses que, culminou na finalizacao do mesmo, e na  entregar um algoritmo potencialmente utilizável.
  */
 
 #include<iostream>
@@ -23,183 +16,219 @@ using std::cin;
 using std::cout;
 using std::endl;
 
-#include<string>
+#include<cstring>
 using std::string;
 using std::getline;
+using std::strcpy;
+
+#include<stdlib.h>
 
 #include <iomanip>
-using std::setw; // usado para ajustar a largura no modo de exibicao
+// usado para ajustar a largura no modo de exibicao
+using std::setw; 
+
+//Capacidade de amarzenamento do vetor cadastro
+#define capacity 100 
 
     struct Carro
     {
-        int id; 
-        string nome;
+        char placa[10]; 
+        string modelo;
         string marca;
-        int ano_lancmento;
+        short ano_lancmento;
         string descricao;
 
     };
-    void message();
+
+    /*********PROTOTIPO DAS FUNCOES***********/
+    void menu();
+    void insert ();
+    void exibir();
+    void alterar ();
     void sair ();
-    void insert ( )
+    void delay();
+    
+    
+    /*************FUNCAO PRINCIPAL****************/
+      int main()  
     {
-         const int capacity = 3;
+            menu();
+            return 0;
+    } 
+    
+    
+    /*******FUNCAO DE ATRASO DELAY************/
+    void delay(long int tmp) 
+    {
+        for (int time = 0; time < tmp;time++ );
+    }
+    /***********FUNCAO INSERIR*********************/
+    void insert ()
+    {
+
          Carro cadastro[capacity];
-         int terminoCadastro = 0;
         
-      do{// Precisa tratar do metodo
-            for (short i = 0; i < 3; i++){
-                cout <<setw(9);
-                cout << "ID:  ";
-                cin >> cadastro[i].id;
-                terminoCadastro =  cadastro[i].id; // Verifica se e valido e procegue cadastro
-                if (terminoCadastro != -1){
-                
-                    cout <<setw(11);
-                    cout << "NOME:  ";
-                    cin >> cadastro[i].nome;
+        //Inicializa os campos do vetor cadastro
+        for (int i = 0; i < capacity;i++)
+        {
+            strcpy (cadastro[i].placa, "NULL");
+            cadastro[i].modelo = " NULL";
+            cadastro[i].marca = "NULL";
+            cadastro[i].ano_lancmento = 0;
+            cadastro[i].descricao = "NULL";
+        }
+        
+        char op, aux;
+        do{
+                for (short i = 0; i < capacity; i++){
+                    cout << " \tEntre com " << i+1  <<  "o  cadastro.\n";
+                    cout <<setw(10);
+                    cout << "PLACA:  ";
+                    cin >> cadastro[i].placa;
                     
+                    cin.ignore();
+                    cout <<setw(13);
+                    cout << "MODELO:  ";
+                    getline(cin,cadastro[i].modelo);
+                            
                     //cout <<"\n\n";
                     cout <<setw(12);
                     cout << "MARCA:  " ;
-                    cin >> cadastro[i].marca;
-                    cin.ignore();
-                    
-                   // cout <<"\n\n";
+                    getline(cin,cadastro[i].marca);
+                            
+                    // cout <<"\n\n";
                     cout << "  ";
                     cout << "  ANO DE LANCAMENTO:  ";
                     cin >> cadastro[i].ano_lancmento;
+                            
                     cin.ignore();
-                    
                     cout <<setw(16);
                     cout << "DESCRICAO:  ";
                     getline(cin, cadastro[i].descricao);
-                    
-                }else if (terminoCadastro == cadastro[0].id) 
-                {
-                    cout << "\t\tNenhum dado foi inserido.\n";
-                    message();
+                     
+                    cout << "\n\n";
+                    cout << "Digite [1] para continuar e ou valor para sair.\n";
+                    cin >> op;
+                    aux = i;
                 }
-                else
-                    sair();
-                    
-            cout << "\n\n";
-            }
-            
-        }while(terminoCadastro != -1); // Criterio de parada
-    cout << endl <<  "\n" << "\n";
-    }
-        
-    void menu ();
-    void alterar ();
-    void message()
-    {
-        cout << "\t\tDigite [s] para sair e,  \n\t\t[c] para continuar. \n";
+                
+                
+        }while(cin >> op and aux < capacity or op != 1 ); // Criterio de parada  
+        cout << endl 
+                <<  "\n\n";
     
-        cout <<"\n"<<setw(10);
-        char op;
-        cout << "\t";
-        cin >> op;
-        if (op == 'S' or op == 's')
-            sair ();
-        else if(op == 'C' or op == 'c')
-            menu();
-        else{
-            //aguarda alguns milisegundos e sai do programa
-            for (int tmp = 0; tmp < 100000000;tmp++); 
-            cout << "\t";
-            cout <<" Opcao invalida\n"; 
-            sair();
-        }
-    }
-    
+    }    
+    /********************FUNCAO MENU*************************/
     void menu ()
     {
-        cout << "\t\tMENU" << endl;
-        cout << "\n" <<"\n";
         
-        cout << "\t\tEscolha uma opcao: " << endl << endl;
-        cout << "\t\t[1] " << "Cadastrar \n "
-                << "\t\t[2] " << "Buscas \n"
-                << "\t\t[3] " << "Alterar \n"
-                << "\t\t[4] " << "Sair" << "\n\n\n";
-        
-        char opcao;
-        cout << "\t\t";
-        cin >> opcao;
-        cout << "\n\n";
-        switch (opcao)
-        {
-            case '1' :
-                system ("cls");
-                cout << "\t\tEntre com os dados para o cadastro."
-                        << "\n\t\tPara finalizar o cadastro entre com [-1] no campo identificacao - ID\n";
-                insert();
-                cout << "\t\tCadastrado com sucesso."<< endl;
-                message(); // Opcao para sair ou continuar
-                break;
-            case '2' : 
-                // Se cadastro vazio chama metodo inserir
-                // Criar condicao ainda
-                    system("cls");
-                    cout << "\t\tBase de dados vazia.\n"
-                            << "\t\tDeseja inserir dados. " << " [s/n]\n";
-                            cout << "\n";
-                            char op;
-                            cout << "\t\t";
-                            cin >> op;
-                            switch (op)
-                            {
-                               case 's' :
-                               case 'S' :
-                                    insert();
-                                    system("cls");
-                                    cout << "\t\tExecultado com sucesso."<< "\n";
-                                    message();
-                                    break;
-                                case 'n' :
-                                case 'N' :
-                                    sair();
-                                    break;
-                                default :
-                                    cout << "\t\tComando invalido." <<"\n"
-                                            << "\t\tRetornado ao menu principal" << "\n";
-                                for (int tmp = 0; tmp < 100000000; tmp++);
-                                system("cls");
-                                menu();
-                                            
-                            }
-            case '3' :
-                alterar();
-                break;
-            case '4' :
-                sair();
-                break;
-            default:
-                cout << setw(10);
-                cout << "\t\tInsira um comando valido." << "\n";
+         char opcao;
+        do{
                 system("cls");
-                message();
-        }
-        
+                cout << "\t\tMENU" << endl;
+                cout << "\n\n";
+                
+                cout << "\t\tEscolha uma opcao: " << endl << endl;
+                cout << "\t\t[1] " << "Cadastrar \n "
+                        << "\t\t[2] " << "Buscar \n"
+                        << "\t\t[3] " << "Alterar \n"
+                        << "\t\t[4] " << "Excluir \n"
+                        << "\t\t[0] " << "Sair" << "\n\n\n";
+                
+                cout << "\t\t";  cin >> opcao; cout << "\n\n";
+                switch (opcao)
+                {
+                    case '1' :
+                        insert();
+                        break;
+                    case '2' : 
+                        do{
+                                system("cls");
+                                cout << "\t\t[1]  Listar todos. \n"
+                                        << "\t\t[2]  Buscar por item. \n";
+                                        cout << "\n";
+                                        
+                                        cout << "\t\t"; cin >> opcao;
+                                        switch (opcao)
+                                        {
+                                           case '1' :
+                                                cout <<"Listando...\n";
+                                                delay(2000000);
+                                                system("cls");
+                                                break;
+                                            case '2' :
+                                    
+                                                do{
+                                                                system("cls");
+                                                                cout << "\t\t[1]  Buscar por marca.  \n"
+                                                                        << "\t\t[2]  Buscar por modelo. \n"
+                                                                        << "\t\t[3]  Buscar por ano. \n";
+                                                                cout << "\t\t\n";  cin >> opcao;
+                                                                switch (opcao) // MENU DE BUSCA POR ITENS
+                                                                {
+                                                                    case '1' :
+                                                                        cout << "comando1" ;
+                                                                        break;
+                                                                    case '2' :
+                                                                        cout << "comando2" ;
+                                                                        break;
+                                                                    case '3' :
+                                                                        cout << "comando3" ;
+                                                                        break;
+                                                                        
+                                                                    default :
+                                                                        cout << " \t\tOpcao invalida.\n";
+                                                                        delay(2000000);
+                                                                        menu();
+                                                                }
+                                                    }while (opcao);
+                                                        
+                                        }
+                        }while (opcao > 0 and opcao <= 2);
+                    case '3' :
+                    
+                        //exibir();
+                        break;
+                    case '4' :
+                        cout << "Funcao indisponivel no momento. \n"
+                                << "Situacao: < em desenvovimento.... > " <<endl;
+                        delay (200000000); 
+                        break;
+                        
+                    case '5' :
+                        system("exit");
+                        break;
+                     default:
+                        cout << setw(10);
+                        cout << "\t\tInsira um comando valido.\n";
+                        delay(200000000);
+                }
+        } while (opcao > 0 and opcao <= 5);
     }
-    
-    void mostrar()
+    /****************FUNCAO EXIBIR****************/
+    void exibir()
     {
         
-            Carro cadastro[3];
-            for (short i = 0; i < 3 ; i++)
+            Carro cadastro[capacity];
+            cout << setw(15) << " PLACA" 
+                    << setw(15) << " MODELO" 
+                    << setw(15) << " MARCA" 
+                    << setw(21) << " ANO DE LACAMENTO" 
+                    << setw(15) << " DESCRICAO" <<"\n\n";
+                    
+            for (short i = 0; i < capacity ; i++)
             {
-                cout << "ID:  " << cadastro[i].id <<"\n"
-                        <<  "  NOME:    " << cadastro[i].nome << "\n"
-                        <<  "  MARCA:  "  << cadastro[i].marca << "\n"
-                        <<  "  ANO DE LACAMENTO:  " << cadastro[i].ano_lancmento << "\n"
-                        <<  "  DESCRICAO:  " << cadastro[i].descricao << "\n";
+                cout <<  setw(15) << cadastro[i].placa 
+                        <<  setw(15) << cadastro[i].modelo 
+                        <<  setw(15)  << cadastro[i].marca 
+                        <<  setw(21) << cadastro[i].ano_lancmento 
+                        <<  setw(15) << cadastro[i].descricao << endl;
             //  cout <<cadastro[i].descricao.capacity() << endl;
             }
-           cout << "\t";
+           
     }
-    
+    /***************FUCAO ALTERAR******************/
+    // PROCEDIMENTO IMCOMPLETO
     void alterar ()
     {
         
@@ -209,18 +238,11 @@ using std::setw; // usado para ajustar a largura no modo de exibicao
           //  cin >> cadastro[1];
             
     }
+    /***************FUNCAO SAIR*****************/
     void sair ()
     {
         cout <<"\t";
-        cout << "saindo...";
-        system("cls");
+        system("exit");
+        cout << "Finalizado.";
         
     }
-
-
-    int main()  
-    {
-            menu();
-            return 0;
-    }
-
